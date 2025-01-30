ae3ee4ba37bba30c2953c6d00c42952775c552bb
is1807372 <158064535+is1807372@users.noreply.github.com>
2025-01-30 06:00:50 +0300

    Enc function

--- Key.py
+++ Key.py
@@ -9,6 +9,17 @@ def generate_key():
         key = Fernet.generate_key()
         with open("secret.key", "wb") as key_file:
             key_file.write(key)
+            
         print("Encryption key saved as 'secret.key'. Keep this safe!")
     else:
+
         print("Key already exists, skipping key generation.")
+
+
+def decrypt_file(file_path, key):
+    fernet = Fernet(key)
+    with open(file_path, "rb") as file:
+        encrypted_data = file.read()
+    decrypted_data = fernet.decrypt(encrypted_data)
+    with open(file_path, "wb") as file:
+        file.write(decrypted_data)
